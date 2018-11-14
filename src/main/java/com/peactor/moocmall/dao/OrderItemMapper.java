3b6b50f3f428fd8b050c1d42bcccdae8407d795b
frequencehuang <frequencehuang@ylbs.com.cn>
2018-11-14 22:32:45 +0800

    初始化mapper和数据库配置

---
+++ src/main/java/com/peactor/moocmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.peactor.moocmall.dao;
+
+import com.peactor.moocmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
