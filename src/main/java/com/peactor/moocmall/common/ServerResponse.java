4bcd2e27ccab77af5caf268d7d296ef5777b3797
chenjiajun <xgz2732@qq.com>
2018-11-16 14:48:31 +0800

    通用响应对象

--- src/main/java/com/peactor/moocmall/common/ServerResponse.java
+++ src/main/java/com/peactor/moocmall/common/ServerResponse.java
@@ -1,7 +1,10 @@
 package com.peactor.moocmall.common;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
 import java.io.Serializable;
 
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class ServerResponse<T> implements Serializable {
 
     private int status;
@@ -10,5 +13,51 @@ public class ServerResponse<T> implements Serializable {
 
     private T data;
 
+    private ServerResponse(int status) {
+        this.status = status;
+    }
 
+    private ServerResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public static <T> ServerResponse<T> success() {
+        return new ServerResponse<>(ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> ServerResponse<T> success(String msg) {
+        return new ServerResponse<>(ResponseCode.SUCCESS.getCode(), msg);
+    }
+
+    public static <T> ServerResponse<T> success(String msg, T data) {
+        return new ServerResponse<>(ResponseCode.SUCCESS.getCode(), msg, data);
+    }
+
+    public static <T> ServerResponse<T> success(T data) {
+        return new ServerResponse<>(ResponseCode.SUCCESS.getCode(), data);
+    }
+
+    public static <T> ServerResponse<T> error(String msg) {
+        return new ServerResponse<>(ResponseCode.ERROR.getCode(), msg);
+    }
+
+    public static <T> ServerResponse<T> error() {
+        return new ServerResponse<>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getDesc());
+    }
+
+    public static <T> ServerResponse<T> error(int errorCode, String msg) {
+        return new ServerResponse<>(errorCode, msg);
+    }
 }
